0dbd67f1a2a1389049aa10f735c34b44f9321943
Kong TO <newlight77@users.noreply.github.com>
2020-08-05 18:08:18 +0200

    Update PersonManagerTest.java

--- src/test/java/example/PersonManagerTest.java
+++ src/test/java/example/PersonManagerTest.java
@@ -15,7 +15,7 @@ import java.util.List;
  * Created by muhdk on 15/07/2017.
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(PersonManager.class)
+@PrepareForTest({PersonManager.class, PersonStatic.class})
 public class PersonManagerTest {
 
     @Test
@@ -25,13 +25,29 @@ public class PersonManagerTest {
         list.add(new Person("joh", 22));
         int id = 11;
 
-        PowerMockito.when(mock.findPeople(id)).thenReturn(list);
+        PowerMockito.when(mock.find(id)).thenReturn(list);
         PowerMockito.whenNew(PersonDao.class).withAnyArguments().thenReturn(mock);
 
         PersonDao personDao = new PersonDao();
 
         PersonManager personManager = new PersonManager();
-        List<Person> findppl = personManager.findppl(id);
+        List<Person> findppl = personManager.find(id);
+        Assert.assertTrue(findppl.get(0).getName().equals("joh"));
+        Assert.assertTrue(findppl.get(0).getAge()==22);
+
+    }
+
+    @Test
+    public void testStatic() throws Exception {
+        PowerMockito.mockStatic(PersonStatic.class);
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("joh", 22));
+        int id = 11;
+
+        PowerMockito.when(PersonStatic.find(id)).thenReturn(list);
+
+        PersonManager personManager = new PersonManager();
+        List<Person> findppl = personManager.findStatic(id);
         Assert.assertTrue(findppl.get(0).getName().equals("joh"));
         Assert.assertTrue(findppl.get(0).getAge()==22);
 
