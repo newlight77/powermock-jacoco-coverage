effed57ad423bf5de77b41122e290dbf4eb359f1
Kong TO <newlight77@users.noreply.github.com>
2020-08-05 18:09:12 +0200

    Create PersonStatic.java

---
+++ src/main/java/example/PersonStatic.java
@@ -0,0 +1,17 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by muhdk on 15/07/2017.
+ */
+public class PersonStatic {
+
+    public static List<Person> find(final int id) {
+        List<Person> ppl = new ArrayList<>();
+        ppl.add(new Person("smith", 20));
+        ppl.add(new Person("adam", 33));
+        return ppl;
+    }
+}
